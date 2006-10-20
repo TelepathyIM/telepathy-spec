48334d4fe0249e282d6b6729bd213bb0873606e8
Simon McVittie <simon.mcvittie@collabora.co.uk>
2006-10-20 18:17:29 +0000

    Use error classes from _generated
    20061020181729-53eee-146d549683e8032acc597869819c82aa92fe0848.gz

--- telepathy/errors.py
+++ telepathy/errors.py
@@ -19,75 +19,14 @@
 
 import dbus
 
-_TELEPATHY_ERROR_IFACE = 'org.freedesktop.Telepathy.Error'
-
-class NetworkError(dbus.DBusException):
-    """
-    Raised when there is an error reading from or writing to the network.
-    """
-    _dbus_error_name = _TELEPATHY_ERROR_IFACE + '.NetworkError'
-
-class NotImplemented(dbus.DBusException):
-    """
-    Raised when the requested method, channel, etc is not available on this connection.
-    """
-    _dbus_error_name = _TELEPATHY_ERROR_IFACE + '.NotImplemented'
-
-class InvalidArgument(dbus.DBusException):
-    """
-    Raised when one of the provided arguments is invalid.
-    """
-    _dbus_error_name = _TELEPATHY_ERROR_IFACE + '.InvalidArgument'
-
-class NotAvailable(dbus.DBusException):
-    """
-    Raised when the requested functionality is temporarily unavailable.
-    """
-    _dbus_error_name = _TELEPATHY_ERROR_IFACE + '.NotAvailable'
-
-class PermissionDenied(dbus.DBusException):
-    """
-    The user is not permitted to perform the requested operation.
-    """
-    _dbus_error_name = _TELEPATHY_ERROR_IFACE + '.PermissionDenied'
-
-class Disconnected(dbus.DBusException):
-    """
-    The connection is not currently connected and cannot be used.
-    """
-    _dbus_error_name = _TELEPATHY_ERROR_IFACE + '.Disconnected'
-
-class InvalidHandle(dbus.DBusException):
-    """
-    The contact name specified is unknown on this channel or connection.
-    """
-    _dbus_error_name = _TELEPATHY_ERROR_IFACE + '.InvalidHandle'
-
-class ChannelBanned(dbus.DBusException):
-    """
-    You are banned from the channel.
-    """
-    _dbus_error_name = _TELEPATHY_ERROR_IFACE + '.Channel.Banned'
-
-class ChannelFull(dbus.DBusException):
-    """
-    The channel is full.
-    """
-    _dbus_error_name = _TELEPATHY_ERROR_IFACE +'.Channel.Full'
-
-class ChannelInviteOnly(dbus.DBusException):
-    """
-    The requested channel is invite only.
-    """
-    _dbus_error_name = _TELEPATHY_ERROR_IFACE + '.Channel.InviteOnly'
-
-if __name__ == '__main__':
-    def _check():
-        import telepathy._generated.errors as errors
-        for name in globals():
-            if name not in ('_check', '__doc__', '__builtins__',
-                            '__file__', '__name__', 'dbus',
-                            '_TELEPATHY_ERROR_IFACE'):
-                e = getattr(errors, name)
-                assert e._dbus_error_name == globals()[name]._dbus_error_name, name
-    _check()
+from telepathy._generated.errors import \
+        NetworkError, \
+        NotImplemented, \
+        InvalidArgument, \
+        NotAvailable, \
+        PermissionDenied, \
+        Disconnected, \
+        InvalidHandle, \
+        ChannelBanned, \
+        ChannelFull, \
+        ChannelInviteOnly
