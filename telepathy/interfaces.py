3828e3bd753bdb21016a2859e2b9cd1975fd7e90
Simon McVittie <simon.mcvittie@collabora.co.uk>
2006-10-20 18:21:37 +0000

    Use interface constants from _generated
    20061020182137-53eee-49c8170aa80f1efae646c398d4b6414b8b933976.gz

--- telepathy/interfaces.py
+++ telepathy/interfaces.py
@@ -17,44 +17,29 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-CONN_MGR_INTERFACE = 'org.freedesktop.Telepathy.ConnectionManager'
-
-CONN_INTERFACE = 'org.freedesktop.Telepathy.Connection'
-
-CONN_INTERFACE_ALIASING = 'org.freedesktop.Telepathy.Connection.Interface.Aliasing'
-CONN_INTERFACE_AVATARS = 'org.freedesktop.Telepathy.Connection.Interface.Avatars'
-CONN_INTERFACE_CAPABILITIES = 'org.freedesktop.Telepathy.Connection.Interface.Capabilities'
-CONN_INTERFACE_CONTACT_INFO = 'org.freedesktop.Telepathy.Connection.Interface.ContactInfo'
-CONN_INTERFACE_FORWARDING = 'org.freedesktop.Telepathy.Connection.Interface.Forwarding'
-CONN_INTERFACE_PRESENCE = 'org.freedesktop.Telepathy.Connection.Interface.Presence'
-CONN_INTERFACE_PRIVACY = 'org.freedesktop.Telepathy.Connection.Interface.Privacy'
-CONN_INTERFACE_RENAMING = 'org.freedesktop.Telepathy.Connection.Interface.Renaming'
-
-CHANNEL_INTERFACE = 'org.freedesktop.Telepathy.Channel'
-CHANNEL_TYPE_CONTACT_LIST = 'org.freedesktop.Telepathy.Channel.Type.ContactList'
-CHANNEL_TYPE_CONTACT_SEARCH = 'org.freedesktop.Telepathy.Channel.Type.ContactSearch'
-CHANNEL_TYPE_TEXT = 'org.freedesktop.Telepathy.Channel.Type.Text'
-CHANNEL_TYPE_ROOM_LIST = 'org.freedesktop.Telepathy.Channel.Type.RoomList'
-CHANNEL_TYPE_STREAMED_MEDIA = 'org.freedesktop.Telepathy.Channel.Type.StreamedMedia'
-
-CHANNEL_INTERFACE_DTMF = 'org.freedesktop.Telepathy.Channel.Interface.DTMF'
-CHANNEL_INTERFACE_GROUP = 'org.freedesktop.Telepathy.Channel.Interface.Group'
-CHANNEL_INTERFACE_HOLD = 'org.freedesktop.Telepathy.Channel.Interface.Hold'
-CHANNEL_INTERFACE_MEDIA_SIGNALLING = 'org.freedesktop.Telepathy.Channel.Interface.MediaSignalling'
-CHANNEL_INTERFACE_PASSWORD = 'org.freedesktop.Telepathy.Channel.Interface.Password'
-CHANNEL_INTERFACE_TRANSFER = 'org.freedesktop.Telepathy.Channel.Interface.Transfer'
-
-MEDIA_SESSION_HANDLER = 'org.freedesktop.Telepathy.Media.SessionHandler'
-MEDIA_STREAM_HANDLER = 'org.freedesktop.Telepathy.Media.StreamHandler'
-
-PROPERTIES_INTERFACE = 'org.freedesktop.Telepathy.Properties'
-
-if __name__ == '__main__':
-    import telepathy._generated.interfaces as interfaces
-    def _check():
-        for name in globals():
-            if name not in ('interfaces', '_check', '__doc__', '__builtins__',
-                            '__file__', '__name__'):
-                assert getattr(interfaces, name) == globals()[name], (
-                    name, globals()[name], getattr(interfaces, name))
-    _check()
+from telepathy._generated.interfaces import \
+        CONN_MGR_INTERFACE, \
+        CONN_INTERFACE, \
+        CONN_INTERFACE_ALIASING, \
+        CONN_INTERFACE_AVATARS, \
+        CONN_INTERFACE_CAPABILITIES, \
+        CONN_INTERFACE_CONTACT_INFO, \
+        CONN_INTERFACE_FORWARDING, \
+        CONN_INTERFACE_PRESENCE, \
+        CONN_INTERFACE_PRIVACY, \
+        CONN_INTERFACE_RENAMING, \
+        CHANNEL_INTERFACE, \
+        CHANNEL_TYPE_CONTACT_LIST, \
+        CHANNEL_TYPE_CONTACT_SEARCH, \
+        CHANNEL_TYPE_TEXT, \
+        CHANNEL_TYPE_ROOM_LIST, \
+        CHANNEL_TYPE_STREAMED_MEDIA, \
+        CHANNEL_INTERFACE_DTMF, \
+        CHANNEL_INTERFACE_GROUP, \
+        CHANNEL_INTERFACE_HOLD, \
+        CHANNEL_INTERFACE_MEDIA_SIGNALLING, \
+        CHANNEL_INTERFACE_PASSWORD, \
+        CHANNEL_INTERFACE_TRANSFER, \
+        MEDIA_SESSION_HANDLER, \
+        MEDIA_STREAM_HANDLER, \
+        PROPERTIES_INTERFACE
