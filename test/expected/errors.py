ed0202666f12ea35a59d1dd78a6645c31df5d516
Simon McVittie <simon.mcvittie@collabora.co.uk>
2006-11-29 13:04:40 +0000

    Convert docstrings (that need formatting) to XHTML
    20061129130440-53eee-f3c544f2c43c1b8854e81c8e983483946ea5c223.gz

--- test/expected/errors.py
+++ test/expected/errors.py
@@ -1,8 +1,9 @@
-# Generated from the Telepathy spec
-"""\
-    Copyright (C) 2006 Collabora Limited
-  
-    This library is free software; you can redistribute it and/or
+"""Exception classes, generated from the Telepathy spec
+
+Copyright (C) 2006 Collabora Limited
+
+
+This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
 version 2.1 of the License, or (at your option) any later version.
