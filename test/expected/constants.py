40d28152879eac37e34cb374fd32032e19902d6a
Simon McVittie <simon.mcvittie@collabora.co.uk>
2006-12-12 17:24:16 +0000

    Add missing expected data for tests
    20061212172416-53eee-1dd02ac7b0f809a64ef23db8088a09d0a7b0929a.gz

---
+++ test/expected/constants.py
@@ -0,0 +1,24 @@
+"""List of constants, generated from the Telepathy spec version 0.1.2
+
+Copyright (C) 2006 Collabora Limited
+
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+"""
+# TestFlags
+TEST_LOWBIT = 1
+TEST_HIGHBIT = 128
+
+# Adjective
+LEVERAGING = 0
+SYNERGISTIC = 1
+
